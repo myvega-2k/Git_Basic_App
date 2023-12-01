7f43f2fd21686fc7fb47b9df102b02ca77cd243c
myvega-2k <vega2k_@daum.net>
2023-12-01 15:13:05 +0900

    index.js에 multiply함수추가 add함수변경

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 /*
     index.html을 위한 js 파일
 */
-function add(a,b) {
+function add(a,b,c=10) {
+    if(c !== undefined){
+        return a + b + c;
+    }
     return a + b;
+}
+
+function multiply(a, b) {
+    return a * b;
 }
