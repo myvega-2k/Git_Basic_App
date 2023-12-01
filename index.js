4bf5fe620e15c85394e9d416d8ad965fb669f05a
myvega-2k <vega2k_@daum.net>
2023-12-01 15:17:54 +0900

    index.js add함수변경과 substract함수추가

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 /*
     index.html을 위한 js 파일
 */
-function add(a,b) {
-    return a + b;
+function add(a, b, c) {
+    if(c !== undefined){
+        return a + b;
+    }
+}
+
+function substract(a,b) {
+    if(a > b) {
+        return a - b;
+    }else {
+        return b - a;
+    }
 }
